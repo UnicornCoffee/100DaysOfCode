0aef647c7fd5985ccf2d8aa4d1b3cec0cde3d938
UnicornCoffee <mikeyschemicalunicorn@gmail.com>
2023-05-24 14:53:11 +0100

    Add files via upload

---
+++ bandnamegen.py
@@ -0,0 +1,6 @@
+print ('Hello! Welcome to the generator!')
+citynme = input("What city did you grow up in? \n")
+
+petnme = input("What was the name of your first pet? \n")
+
+bandnme = print(citynme + petnme)
